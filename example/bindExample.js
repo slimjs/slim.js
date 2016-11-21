59ea002d3fb471cd6231653606cbd31289b475ab
eavichay <eavichay@gmail.com>
2016-11-21 13:18:12 +0200

    binding by bind-id

--- example/bindExample.js
+++ example/bindExample.js
@@ -1,16 +1,19 @@
 (function(document) {
     "use strict";
 
-    const template = `<input type="text" value="@{customValue}"></input>`
+    const template = `<input type="text" value="@{customValue}" custom="@{anotherValue}"></input>`
 
     document.registerElement('example-bind', class extends SlimBaseElement {
 
         constructor() {
             super()
+
         }
 
         render() {
             this.innerHTML = template;
+            this.customValue = "12345"
+            this.anotherValue = "5678"
         }
 
     })
