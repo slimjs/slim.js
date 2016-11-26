0905f1f0b332ed663dd601168133ce11b168fc3a
eavichay <eavichay@gmail.com>
2016-11-26 22:51:32 +0200

    added method binding, simplified code for property binding

--- example/bindExample.js
+++ example/bindExample.js
@@ -1,23 +1,19 @@
 (function(document) {
     "use strict";
 
-    const template = `<input type="text" value="@{customValue}" custom="@{anotherValue}"></input>`
-
     document.registerElement('example-bound', class extends SlimBaseElement {
 
-        get _renderOnAttributes() {
-            return ['my-number'];
-        }
 
         _onAttributeChanged(attribute, oldValue, newValue) {
             if (attribute === 'my-number') {
                 this.myNumber = newValue
+                this.render()
             }
             super._onAttributeChanged(attribute, oldValue, newValue)
         }
 
         render() {
-            this.innerHTML = `<p>My lucky number is ${this.myNumber}</p>`
+            this.innerHTML = `<p>My lucky number is ${this.getAttribute('my-number')}</p>`
         }
 
     })
--- example/parentExample.js
+++ example/parentExample.js
@@ -3,16 +3,23 @@
 
     document.registerElement('example-parent', class extends SlimBaseElement{
 
-        _onCreated() {
-            this.myProp = 1
-            this._render()
+        _onAdded() {
+            this.number1 = 1;
+            this.number2 = 5;
+            this._render();
+        }
+
+        reverse(what, what2) {
+            return what.toString().split('').reverse().join('') + '____' + what2
         }
 
         render() {
-            this.innerHTML = `<example-bound my-number="@{myProp}"></example-bound><button id="myBtn">Click me</button>`
+            this.innerHTML = `
+<example-bound my-number="@{number1}"></example-bound>
+<button id="myBtn">Click me:</button>
+<example-bound my-number="@{reverse(number1, number2)}"></example-bound>`
             this.find('#myBtn').onclick = function() {
-                this.myProp = Math.random()
-                console.log(this.myProp)
+                this.number1 = Math.random()
             }.bind(this)
         }
         myMethod(what) {
