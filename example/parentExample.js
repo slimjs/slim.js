146512ad9a3e81243c3af5ec32b3b8531f7328ed
eavichay <eavichay@gmail.com>
2016-11-27 14:13:01 +0200

    Added modal example with buttons

--- example/parentExample.js
+++ example/parentExample.js
@@ -7,6 +7,10 @@
             this.number1 = 1;
             this.number2 = 5;
             this._render();
+
+            this.modalContent = document.createElement('div');
+            this.modalContent.innerHTML = `<p>Hello</p>
+            <button id="close-modal">Close</button><button id="do-something">Say something</button>`
         }
 
         reverse(what, what2) {
@@ -17,10 +21,24 @@
             this.innerHTML = `
 <example-bound my-number="@{number1}"></example-bound>
 <button id="myBtn">Click me:</button>
-<example-bound my-number="@{reverse(number1, number2)}"></example-bound>`
+<example-bound my-number="@{reverse(number1, number2)}"></example-bound>
+<hr/>
+<button id="mdl">Modal</button>`
             this.find('#myBtn').onclick = function() {
                 this.number1 = Math.random()
                 this.number2 = Math.random();
+            }.bind(this)
+
+            this.find('#mdl').onclick = function() {
+                let modal = createModal()
+                modal.attachDOM(this.modalContent)
+                modal.show()
+                modal.find('#close-modal').onclick = function() {
+                    modal.hide()
+                }
+                modal.find('#do-something').onclick = function() {
+                    alert('something!');
+                }
             }.bind(this)
         }
         myMethod(what) {
