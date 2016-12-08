522c3b7a33e5b79bf66b9d58576de1e8c6847f6c
eavichay <eavichay@gmail.com>
2016-12-08 17:45:42 +0200

    merging files into main

--- SlimElement.js
+++ SlimElement.js
@@ -378,6 +378,98 @@
 
         }
 
+        Slim('s-states', class extends SlimBaseElement {
+
+            get updateOnAttributes() {
+                return ['current-state']
+            }
+
+            get currentState() {
+                return this.getAttribute('current-state');
+            }
+
+            set currentState(value) {
+                if (value !== this.currentState) {
+                    this.setAttribute('current-state')
+                }
+            }
+
+            beforeRender() {
+                this.virtual = document.createDocumentFragment()
+                this.actual = this.__bindingTree
+                this.currentState = this.getAttribute('current-state') || ''
+            }
+
+            afterRender() {
+                this.update()
+            }
+
+            update() {
+                for (let i = this.actual.children.length; i > 0; i--) {
+                    let child = this.actual.children[i - 1];
+                    if (child.getAttribute('in-state') !== this.currentState) {
+                        this.actual.removeChild(child)
+                        this.virtual.appendChild(child)
+                    }
+                }
+
+                for (let i = this.virtual.children.length; i > 0; i--) {
+                    let child = this.virtual.children[i - 1];
+                    if (child.getAttribute('in-state') === this.currentState) {
+                        this.virtual.removeChild(child)
+                        this.actual.appendChild(child)
+                    }
+                }
+            }
+
+        })
+
+        Slim('s-repeat', class SlimRepeat extends SlimBaseElement {
+
+            get sourceData() {
+                try {
+                    return this.parentBind[this.getAttribute('source')]
+                }
+                catch (err) {
+                    return []
+                }
+            }
+
+            _renderCycle() {
+                super._renderCycle(true)
+            }
+
+            update() {
+                this.innerHTML = ''
+                let childrenToAdd = []
+                for (let dataItem of this.sourceData) {
+                    for (let child of this.__bindingTree.children) {
+                        let node = child.cloneNode(true)
+                        node.parentBind = node
+                        node.data = dataItem
+                        if (!node.parentBind) {
+                            node.parentBind = node
+                        }
+                        for (let prop in dataItem) {
+                            node[prop] = dataItem[prop]
+                            if (!(typeof dataItem[prop] === "function") && !(typeof dataItem[prop] === "object")) {
+                                node.setAttribute(prop, dataItem[prop])
+                            }
+                        }
+                        if (node.isSlim) {
+                            node.createdCallback(true)
+                        } else {
+                            this._applyTextBindings.bind(node)()
+                        }
+                        childrenToAdd.push(node)
+                    }
+                }
+                for (let child of childrenToAdd) {
+                    this.appendChild(child)
+                }
+            }
+        })
+
         Slim('slim-component', class extends Element{
 
                 update() {
