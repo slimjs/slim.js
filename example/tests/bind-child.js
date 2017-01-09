eb9c8b0e25aa55e338bb50bd3c07ed2875c02ea8
eavichay <eavichay@gmail.com>
2017-01-09 09:02:45 +0200

    2.2.3

--- example/tests/bind-child.js
+++ example/tests/bind-child.js
@@ -1,18 +1,30 @@
 Slim.tag('bind-child', class extends Slim {
 
     get template() {
-        return `<div bind>[[data.label]] Click for value</div>`
+        return `<div bind>[[data.label]] Click for value [[updateCount]] / [[renderCount]]</div>`
     }
 
     onBeforeCreated() {
+        this.updateCount = 0;
+        this.renderCount = 0;
         this.customRender = false;
+    }
+
+    update() {
+        this.updateCount++
+        super.update()
+    }
+
+    render(tpl) {
+        this.renderCount++
+        super.render(tpl)
     }
 
     onAfterRender() {
         this.onclick = ()=>{
             this.customRender = !this.customRender;
             if (this.customRender) {
-                this.render(`<div prop="[[data.label]]" bind>[[data.label]] : [[data.value]]</div>`)
+                this.render(`<div prop="[[data.label]]" bind>[[data.label]] : [[data.value]] - [[updateCount]] / [[renderCount]]</div>`)
             } else {
                 this.render()
             }
