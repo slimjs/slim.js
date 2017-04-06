dac61eb5cebdf42e1e40df99d5729cc6fbedb53d
eavichay <eavichay@gmail.com>
2017-04-07 02:51:20 +0300

    test script

--- tests/binding.test.js
+++ tests/binding.test.js
@@ -10,7 +10,17 @@ module.exports = {
     binding: function(browser) {
         browser.waitForElementVisible('h1');
         browser.assert.containsText('h1', 'Hello, eavichay');
+        browser.assert.attributeEquals('#check-prop', 'attr', 'eavichay');
+        browser.assert.attributeEquals('#check-method', 'attr', 'yahcivae');
         browser.click('h1');
         browser.assert.containsText('h1', 'Hello, slim.js');
+        browser.assert.attributeEquals('#check-prop', 'attr', 'slim.js');
+        browser.assert.attributeEquals('#check-method', 'attr', 'sj.mils');
+        browser.execute(`
+            document.querySelector('test-binding').myName = 'test';
+        `);
+        browser.assert.containsText('h1', 'Hello, test');
+        browser.assert.attributeEquals('#check-prop', 'attr', 'test');
+        browser.assert.attributeEquals('#check-method', 'attr', 'tset');
     }
 };
