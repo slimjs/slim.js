57076b1b73fa0968c049a0b7eb2240e4fe747609
eavichay <eavichay@gmail.com>
2017-04-07 23:01:57 +0300

    removing old and unused files

--- example/webpage/slim-logo.js
+++
@@ -1,7 +0,0 @@
-Slim.tag('slim-logo', class extends Slim {
-
-
-    get template() { return `<div><img slim-id="img" src="slim-icon-3.png" width="128" height="183" /><content></content></div>`}
-
-
-})
