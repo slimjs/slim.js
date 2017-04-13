1967990792cd32fd08fcc0f798640ffb865588e2
eavichay <eavichay@gmail.com>
2017-04-13 21:39:07 +0300

    decorators update

--- Decorators.ts
+++ Decorators.ts
@@ -1,10 +1,10 @@
-export function tag(selector:string) {
+export function tag(selector:string):Function {
     return function(target:any):void {
         window['Slim'].tag(selector, target);
     };
 }
 
-export function template(tpl:string) {
+export function template(tpl:string):Function {
     return function(target:any):void {
         target.prototype.__defineGetter__('template', () => {
             return tpl;
@@ -12,7 +12,7 @@ export function template(tpl:string) {
     }
 }
 
-export function useShadow(value:boolean) {
+export function useShadow(value:boolean):Function {
     return function(target:any):void {
         target.prototype.__defineGetter__('useShadow', () => {
             return value;
