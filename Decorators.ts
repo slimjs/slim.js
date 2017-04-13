ea6019fb3feab6a92e484ae96bf933bbfb07e317
Avichay Eyal <eavichay@gmail.com>
2017-04-13 15:37:18 +0300

    Adding decorators

---
+++ Decorators.ts
@@ -0,0 +1,21 @@
+export function tag(selector:string) {
+    return function(target:any):void {
+        window['Slim'].tag(selector, target);
+    }
+}
+
+export function template(tpl:string) {
+    return function(target:any):void {
+        target.prototype.__defineGetter__('template', () => {
+            return tpl;
+        });
+    }
+}
+
+export function useShadow(value:boolean) {
+    return function(target:any):void {
+        target.prototype.__defineGetter__('useShadow', () => {
+            return value;
+        })
+    }
+}
