afb05a0faf793d5dc64d3da4ecafbbe3d2a732eb
Avichay Eyal <eavichay@gmail.com>
2017-04-13 13:57:13 +0300

    Typings for Typescript

---
+++ Slim.d.ts
@@ -0,0 +1,34 @@
+export declare class Slim {
+    static polyfill(url:string):void;
+    static tag(tag:string, template:string, clazz:any):void;
+    static getTag(clazz:Slim):string;
+    static plugin(phase:string, plugin:Function):void;
+    static registerCustomAttribute(attr:string, fn:Function):void;
+
+    createdCallback():void;
+    attachedCallback():void;
+    detachedCallback():void;
+    connectedCallback():void;
+    disconnectedCallback():void;
+
+    onBeforeCreated():void;
+    onCreated():void;
+    onAdded():void;
+    onRemoved():void;
+    onBeforeRender():void;
+    onAfterRender():void;
+    onBeforeUpdate():void
+    onAfterUpdate():void;
+
+    update():void;
+
+    render(template?:string):void;
+
+    find(selector:string):HTMLElement;
+    findAll(selector:string):HTMLElement[];
+    watch(prop:string, executor:Function):void;
+    callAttribute(attributeName:string, value:any);
+    useShadow:boolean;
+    isInteractive:boolean;
+    template:string;
+}
