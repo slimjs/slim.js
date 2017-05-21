bfc11e496394cbbebf1d73ff8d946ba761f3862a
eavichay <eavichay@gmail.com>
2017-05-21 22:53:33 +0300

    slim3 wip

--- src/Slim3.js
+++ src/Slim3.js
@@ -13,6 +13,56 @@ class PropertyBinding {
     }
 }
 
+class PropertyMethodBinding {
+    constructor(source, target, attributeName, rxMethod) {
+        this.source = source;
+        this.target = target;
+        this.attributeName = attributeName;
+        this.methodName = rxMethod[1];
+        this.expressions = rxMethod[3].replace(' ', '').split(',');
+        this.properties = this.expressions.map( expression => {
+            return expression.split('.')[0];
+        });
+    }
+
+    execute() {
+        const values = this.expressions.map( expression => {
+            return Slim.__lookup(this.source, expression).obj
+        });
+        const value = this.source[ this.methodName ].apply( this.source, values );
+        this.target.setAttribute(this.attributeName, value);
+        this.target[Slim.__dashToCamel(this.attributeName)] = value;
+    }
+}
+
+class TextBinding {
+    constructor(source, target, sourceInnerText) {
+        this.source = source;
+        this.target = target;
+        this.sourceInnerText = sourceInnerText;
+    }
+    execute() {
+        this.target.innerText = this.sourceInnerText;
+        let match = this.sourceInnerText.match(Slim.rxTextMethod);
+        match && match.forEach( expression => {
+            const matches = expression.match(Slim.rxMethod);
+            const methodName = matches[1];
+            const props = matches[3].split(' ').join('').split(',');
+            const values = props.map( expr => {
+                return Slim.__lookup(this.source, expr ).obj;
+            });
+            const value = this.source[ methodName ].apply( this.source, values );
+            this.target.innerText = this.target.innerText.replace(expression, value);
+        });
+        match = this.target.innerText.match(Slim.rxTextProp);
+        match && match.forEach( expression => {
+            const deepProperty = expression.match(Slim.rxProp)[1];
+            const value = Slim.__lookup(this.source, deepProperty).obj;
+            this.target.innerText = this.target.innerText.replace(expression, value);
+        });
+    }
+}
+
 class Slim extends HTMLElement {
 
     /**
@@ -105,27 +155,109 @@ class Slim extends HTMLElement {
         });
     }
 
+    static executeBindings(target) {
+        target._root && target._root.querySelectorAll('*').forEach( node => {
+            if (node._bindings) {
+                Object.values(node._bindings).forEach( binding => {
+                    binding.forEach( bind => bind.execute() );
+                });
+            }
+        });
+    }
+
+    static executeSingleBindings(target, property) {
+        target._bindings && target._bindings[property] && target._bindings[property].forEach( executor => {
+            executor();
+        });
+    }
+
     static captureBindings(target) {
         target._root && target._root.querySelectorAll('*').forEach(child => {
             for (let i = 0; i < child.attributes.length; i++) {
                 const attribute = child.attributes[i];
+
+                if (attribute.nodeName === 'bind') {
+                    let expressions = {};
+                    const tProp = Slim.rxTextProp.exec(child.innerText);
+                    const mProp = Slim.rxTextMethod.exec(child.innerText);
+                    tProp && tProp.forEach( match => {
+                        const breakDown = match.match(Slim.rxProp);
+                        if (breakDown) {
+                            const prop = breakDown[1];
+                            expressions[prop] = true;
+                        }
+                    });
+                    mProp && mProp.forEach( match => {
+                        const breakDown = match.match(Slim.rxMethod);
+                        if (breakDown) {
+                            const props = breakDown[3].split(' ').join('').split(',');
+                            props.forEach( prop => {
+                                expressions[prop] = true;
+                            })
+                        }
+                    });
+                    Object.keys(expressions).forEach(expression => {
+                        const property = expression.split('.')[0];
+                        Slim.initBindings(target, child, property);
+                        const bind = new TextBinding(target, child, child.innerText);
+                        child._bindings[property].push(bind);
+                    });
+                    continue;
+                }
                 const isProp = Slim.rxProp.exec(attribute.nodeValue);
+                const isMProp = Slim.rxMethod.exec(attribute.nodeValue);
 
                 if (isProp) {
                     const expression = isProp[1];
                     const property = expression.split('.')[0];
                     const bind = new PropertyBinding(target, child, attribute.nodeName, expression);
-                    Slim.initBindings(child, property);
+                    Slim.initBindings(target, child, property);
                     child._bindings[property].push(bind);
+                    bind.execute();
+                    continue;
+                }
+
+                if (isMProp) {
+                    const bind = new PropertyMethodBinding(target, child, attribute.nodeName, isMProp);
+                    bind.properties.forEach( property => {
+                        Slim.initBindings(target, child, property);
+                        child._bindings[property].push(bind);
+                        bind.execute();
+                    });
+                    continue;
                 }
             }
         });
     }
 
-    static initBindings(target, name) {
+    static initBindings(source, target, name) {
         target._bindings = target._bindings || {};
+        source._bindingsValues = source._bindingsValues || {};
+        source.__returnAsIs = source.__returnAsIs || function(x) { return x; }
         if (name) {
             target._bindings[name] = target._bindings[name] || [];
+            const originalSetter = source.__lookupSetter__(name);
+            let newSetter;
+            if (!originalSetter) {
+                newSetter = function(x) {
+                    this._bindingsValues[name] = x;
+                    Slim.executeBindings(this);
+                };
+                newSetter.isBindingSetter = true;
+            } else if (!originalSetter.isBindingSetter) {
+                newSetter = function(x) {
+                    this._bindingsValues[name] = x;
+                    originalSetter.call(this, x);
+                    Slim.executeBindings(this);
+                };
+            }
+            if (newSetter) {
+                source._bindingsValues[name] = source[name];
+                source.__defineSetter__(name, newSetter);
+                source.__defineGetter__(name, function() {
+                    return this._bindingsValues[name];
+                });
+            }
         }
     }
 
@@ -144,6 +276,7 @@ class Slim extends HTMLElement {
         this.onBeforeRender();
         Slim.render(this);
         Slim.captureBindings(this);
+        Slim.executeBindings(this);
         this.onRender();
     }
 
