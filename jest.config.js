3135ffa102a1e13b9ff51d878d9d3a088c1e83e3
Martijn Jacobs <maerteijn@gmail.com>
2021-11-10 11:24:31 +0100

    Use the jest JSDOM plugin

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,8 @@
-import('jsdom');
-import 'jsdom-global/register.js';
 
 export default async () => {
-  const SlimModule = await import('./dist/index.js');
-  global.Slim = SlimModule.Slim;
   return {
     testPathIgnorePatterns: ['/old-src/'],
     testURL: 'http://localhost/',
-    testEnvironment: 'jsdom',
-    globals: {
-      window: {},
-    },
+    testEnvironment: 'jsdom'
   };
 };
--- tests/__init__.spec.ts
+++ tests/__init__.spec.ts
@@ -1,11 +1,5 @@
 import { Slim } from '../dist/index';
-import 'jsdom-global';
 
-beforeAll(() => {
-  console.log(window);
-  global.Slim = window.Slim;
-  console.log(Slim);
-});
 
 describe('Slim Module', () => {
   it('Should place on window', () => {
